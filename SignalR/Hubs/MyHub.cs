3c0e959510cb2697a604e4e5bca7d664c51f07bc
Rahib M. Cafarov <CafarovR@kapitalho.az>
2021-11-21 22:01:40 +0400

    first

---
+++ SignalR/Hubs/MyHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class MyHub : Hub
+    {
+        static List<string> Users = new List<string>();
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("receiveMessage",message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            Users.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("userJoined", Users);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            Users.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("userLeft", Users);
+        }
+    }
+}
