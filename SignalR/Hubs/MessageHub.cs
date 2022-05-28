818090ccb85d40b493e53d3ddbe5684203415f18
= <=>
2022-05-28 16:50:56 +0400

    Hub Client methods

--- SignalR/Hubs/MessageHub.cs
+++ SignalR/Hubs/MessageHub.cs
@@ -9,7 +9,7 @@ namespace SignalR.Hubs
     public class MessageHub : Hub
     {
         
-        public async Task SendMessageAsync(string message)
+        public async Task SendMessageAsync2(string message, IEnumerable<string>connectionIds)
         {
             #region All
             //await Clients.All.SendAsync("receiveMessage", message);
@@ -18,8 +18,28 @@ namespace SignalR.Hubs
             //await Clients.Caller.SendAsync("receiveMessage", message);
             #endregion
             #region Other
-            await Clients.Others.SendAsync("receiveMessage", message);
+            //await Clients.Others.SendAsync("receiveMessage", message);
             #endregion
+            #region AllExcept
+            //await Clients.AllExcept(connectionIds).SendAsync("receiveMessage", message);
+            #endregion
+            #region Client
+            //await Clients.Client(connectionIds.First()).SendAsync("receiveMessage", message);
+            #endregion
+            #region Clients
+            await Clients.Clients(connectionIds).SendAsync("receiveMessage", message);
+            #endregion
+        }
+
+        public async Task SendMessageAsync(string message, string groupName)
+        {
+            #region Group
+            await Clients.Group(groupName).SendAsync("receiveMessage", message);
+            #endregion
+        }
+        public async Task AddGroup(string connectionId, string groupName)
+        {
+            await Groups.AddToGroupAsync(connectionId, groupName);
         }
         public override async Task OnConnectedAsync()
         {
